fixed factor of 2 for C_2

main.py
@@ -10,8 +10,8 @@ from utils.io import write_dict
 model = delta_chi
 # gamma = 1
 
-rslist = np.arange(0.25, 10.25, 0.25)
-inverse = 1
+rslist = np.arange(1.25, 10.25, 0.25)
+inverse = 0
 
 print(f"Fitting X with {model.__name__}...")
 start_time = time.time()

src/utils/physics.py
@@ -59,7 +59,7 @@ def chi_moment(n, rs):
     if n == 1:
         return 3 / (8 * np.pi**2)
     if n == 2:
-        return 15 / (16 * np.pi * kF) + 15 * f0 / (16 * np.pi**3)
+        return 15 / (8 * np.pi * kF) + 15 * f0 / (8 * np.pi**3)
     if n == 3:
 
         def f1_corradini(rs, dq):